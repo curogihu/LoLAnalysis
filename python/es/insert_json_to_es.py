b0568dec88cab2c5321a96af0a8ce8db8f4e2d24
curogihu <curoasg@gmail.com>
2017-07-20 12:11:57 +0900

    modify to add directly json data to es

--- python/es/insert_json_to_es.py
+++ python/es/insert_json_to_es.py
@@ -3,27 +3,45 @@ import glob
 import requests
 from elasticsearch import Elasticsearch
 
+"""
+import os, sys
+print(os.getcwd())
+sys.path.append(os.getcwd())
+"""
+
+import .utility
+import .apiKey as a
+
 es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 
 # jsonFiles = glob.glob("---------/game/info/*.*")
 
 
+with open("../output/list/gameIds.csv") as fGameIds:
+    gameIds = fGameIds.readlines()
+
+cnt = 0
+gameIdsLen = len(gameIds)
+
 i = 1
 
-"""
-for jsonFile in jsonFiles:
-#    print(jsonFile)
-    with open(jsonFile, 'r') as f:
-        # gameJson = json.load(f)
-        es.index(index='game_info', doc_type='game_info', id=i, body=json.loads(gameJson))
-        i += 1
+for gameId in gameIds:
+    gameId = gameId.replace("\n", "")
 
-        print(1)
-"""
+    print("expected gameId json = " + gameId)
+    gameInfoJson = utility.getLoLGameInfoJson(utility.gameInfoUrl, str(gameId))
 
+    url = utility.gameInfoUrl.replace("")
+    url = url.replace("[GAMEID]", gameId)
+    url = url.replace("[APIKEY]", a.apiKey)
 
-r = requests.get('https://na1.api.riotgames.com/lol/match/v3/matches/2508434907?api_key=RGAPI-47f731a0-65be-4e8b-b4aa-5cc9ba8fbf3d')
-es.index(index='lol_game_info', doc_type='game_info', id=i, body=json.loads(r.content))
+    r = requests.get(url)
+    es.index(index='lol_game_info', doc_type='game_info', id=i, body=json.loads(r.content))
 
+    if cnt % 10 == 0:
+        print(str(cnt) + " / " + str(gameIdsLen) + " " + datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
+
+    cnt+=1
+    i+=1
 
 print("finished")

3c560d6ada9061778fbafb0b6d238fa1db3932e4
curogihu <curoas@yahoo.co.jp>
2017-07-23 20:51:13 +0900

    refactoring part 4 + start writing for getting timeline information

--- python/getGameTimelineJson.py
+++ python/getGameTimelineJson.py
@@ -2,34 +2,32 @@ import utility
 import json
 from datetime import datetime
 
-with open("../output/list/game_ids.csv") as fGameIds:
-    gameIds = fGameIds.readlines()
+with open(utility.gameids_file_path) as f_game_ids:
+    game_ids = f_game_ids.readlines()
 
 cnt = 0
-gameIdsLen = len(gameIds)
+game_ids_len = len(game_ids)
 
-for gameId in gameIds:
-    gameId = gameId.replace("\n", "")
+for game_id in game_ids:
+    game_id = game_id.replace("\n", "")
 
-    print("expected game_id json = " + gameId)
-    gameTimelineJson = utility.getLoLGameTimelineJson(utility.gameTimelineDirectoryPath, str(gameId))
+    print("expected game_id json = " + game_id)
+    game_info_json = utility.get_lol_game_info_json(utility.game_info_url, str(game_id))
 
-    if gameTimelineJson == "" or gameTimelineJson == "429":
-        print("skipped summonerId json = " + gameId)
-        exit()
+    if game_info_json == "" or game_info_json == "429":
+        print("skipped summonerId json = " + game_id)
+        continue
 
     cnt += 1
 
     if cnt % 10 == 0:
-        print(str(cnt) + " / " + str(gameIdsLen) + " " + datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
+        print(str(cnt) + " / " + str(game_ids_len) + " " + datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
 
+    print(utility.game_info_directory_path + game_id + ".json")
 
-
-    print(utility.gameTimelineDirectoryPath + gameId + ".json")
-
-    with open(utility.gameTimelineDirectoryPath + gameId + ".json", "w") as fJson:
+    with open(utility.game_info_directory_path + game_id + ".json", "w") as f_json:
         try:
-            json.dump(gameTimelineJson, fJson, separators=(',', ': '))
+            json.dump(game_info_json, f_json, separators=(',', ': '))
         except UnicodeEncodeError as e:
-            print("UnicodeEncodeError [getMatchjson] game_id = " + gameId)
+            print("UnicodeEncodeError [getMatchjson] game_id = " + game_id)
             # give up getting json

36d8a0c3da1fc3ad16b6f6d6a142a396b17e62e8
curogihu <curoasg@gmail.com>
2017-07-27 09:21:48 +0900

    add python to collect data

---
+++ python/collectInformation/getChampionInfo.py
@@ -0,0 +1,20 @@
+"https://jp1.api.riotgames.com/lol/static-data/v3/champions?locale=ja_JP&dataById=true&api_key=[APIKEY]"
+
+{
+    data: {
+        1: {
+            id: 1,
+            key: "Annie",
+            name: "アニー",
+            title: "闇の申し子"
+        },
+        1: {
+            id: 1,
+            key: "Annie",
+            name: "アニー",
+            title: "闇の申し子"
+        }
+    },
+    type: "champion",
+    version: "7.15.1"
+}
---
+++ python/collectInformation/getItemInfo.py
@@ -0,0 +1,20 @@
+"https://jp1.api.riotgames.com/lol/static-data/v3/items?locale=ja_JP&api_key=[APIKEY]"
+
+{
+    data: {
+        1001: {
+            id: 1001,
+            name: "ブーツ",
+            description: "<groupLimit>複数所持不可。</groupLimit><br><br><unique>自動効果(重複不可) - 移動速度強化:</unique> 移動速度 +25",
+            plaintext: "移動速度がわずかに上昇"
+        },
+        3903: {
+            id: 3903,
+            name: "士気上昇",
+            description: "500シルバーサーペントでアップグレード可能。<br><br><unique>自動効果(重複不可):</unique> 砲撃範囲内の味方は、2秒間移動速度が30%増加する。",
+            plaintext: "「一斉砲撃」により味方の移動速度が上昇する"
+        }
+    },
+    type: "item",
+    version: "7.15.1"
+}

6abbc12f67b995f2afcc154f499bb2fa9dfd868e
curogihu <curoasg@gmail.com>
2017-08-07 08:48:00 +0900

    create template files

--- django/challenger_item_build/challenger_item_build/settings.py
+++ django/challenger_item_build/challenger_item_build/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'challenger_item_build.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
